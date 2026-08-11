85acf0410a4d3327f926d45182596a2eb7ddb51a
Saba <purtseladzesaba@gmail.com>
2026-08-11 13:33:57 +0400

    added player, computer and draw scores

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,11 +11,26 @@ import { Component, signal, computed } from '@angular/core';
     <p>შენ: {{ player() }}</p>
     <p>BOT ოპონენტი: {{ computer() }}</p>
     <h1>{{ result() }}</h1>
+    <h4>Player Score: {{ playerScore() }}</h4>
+    <h4>Computer Score: {{ computerScore() }}</h4>
+    @if (drawCount() > 0) {
+      <h5>Draw Count: {{ drawCount() }}</h5>
+    }
+    @if (playerScore() - computerScore() > 10) {
+      <h5>ყოჩაღ!! შენ იგებ დიდი ანგარიშით მეტოქის წინააღმდეგ</h5>
+    } @else {}
+    @if (computerScore() - playerScore() > 10) {
+      <h5>სამწუხაროა. შენ აგებ დიდი ანგარიშით მეტოქის წინააღმდეგ</h5>
+    }
   `,
 })
 export class App {
   player = signal('');
   computer = signal('');
+
+  playerScore = signal(0);
+  computerScore = signal(0);
+  drawCount = signal(0);
 
   result = computed(() => {
     const p = this.player();
@@ -37,5 +52,18 @@ export class App {
     const options = ['ქვა', 'ფურცელი', 'მაკრატელი'];
     this.player.set(choice);
     this.computer.set(options[Math.floor(Math.random() * options.length)]);
+    console.log(this.result());
+    if (this.result() == 'შენ გაიმარჯვე 👍🏻') {
+      this.playerScore.update((value) => value + 3);
+    }
+    if (this.result() == 'შენ წააგე 👎🏻') {
+      this.computerScore.update((value) => value + 3);
+    }
+    if (this.result() == 'ნიჩია 🙌🏻') {
+      this.playerScore.update((value) => value + 1);
+      this.computerScore.update((value) => value + 1);
+
+      this.drawCount.update((value) => value + 1);
+    }
   }
 }
